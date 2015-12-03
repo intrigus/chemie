6cfde88460060ac5bfb3b0a285c259cf51bde472
intrigus <abc123zeus@live.de>
2015-12-03 22:03:13 +0100

    Initial Commit

---
+++ android/src/main/kotlin/de/intrigus/chem/ChemieActivity.kt
@@ -0,0 +1,34 @@
+package de.intrigus.chem
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class ChemieActivity : AppCompatActivity() {
+private val counterStore = CounterStore()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
+        supportFragmentManager.beginTransaction().replace(R.id.content, TestFragment()).commit()
+    }
+}
+
+class TestFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view: View = inflater!!.inflate(R.layout.test, container, false)
+        return view
+    }
+}
